75867c5125d07914e3103cbc1cfc29baa26eb422
SHIRO <39456646+shiro46mt@users.noreply.github.com>
2026-02-11 22:47:47 +0900

    init

---
+++ main.py
@@ -0,0 +1,62 @@
+from io import StringIO
+from pathlib import Path
+import re
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def download():
+    url = 'https://survey.tmiph.metro.tokyo.lg.jp/epidinfo/weeklyhc.do'
+    payloads = {
+        'periodOffset': '1',
+        'val(prefCode)': '13',
+        'val(hcCode)': '00',
+        'val(epidCode)': '',
+        'val(refMode)': '',
+        'val(refYear)': '',
+        'val(showFp)': '1',
+        'val(year)': 2999,
+        'val(week)': 53,
+    }
+
+    response = requests.post(url, data=payloads)
+    if not response.ok:
+        return False
+    soup = BeautifulSoup(response.text, features="lxml")
+
+    # 年、週番号
+    year = soup.select_one("select[name='val(year)'] > option[selected]").text
+    week_num = soup.select_one("select[name='val(week)'] > option[selected]").text
+
+    # 対象期間
+    target_range = soup.find('td', string=re.compile('対象期間')).text
+    mob = re.search(r"対象期間：\s+(\d+)年(\d+)月(\d+)日 - (\d+)年(\d+)月(\d+)日", target_range)
+    if mob:
+        start_date = '{:0>4s}-{:0>2s}-{:0>2s}'.format(*mob.groups()[:3])
+        end_date = '{:0>4s}-{:0>2s}-{:0>2s}'.format(*mob.groups()[3:])
+    else:
+        start_date, end_date = None, None
+
+    # データ部
+    table = None
+    for tag in soup.select_one('td.epidNameCell').parents:
+        if tag.name == 'table':
+            table = tag
+            break
+    df = pd.read_html(StringIO(str(table).replace('<br/>', '')), header=0)[0].rename(columns={'保健所／疾病名': '保健所'}).query("保健所 != '合計'")
+    cols = list(df.columns)
+    df = df.assign(**{'年': year, '週番号': week_num, '開始日': start_date, '終了日': end_date})[['年', '週番号', '開始日', '終了日'] + cols]
+
+    # 過去データと連結
+    filepath = Path(__file__).parent / 'all.csv'
+    df_all = pd.concat([
+        pd.read_csv(filepath, dtype=str),
+        df
+    ]).drop_duplicates(subset=['年', '開始日', '保健所'])
+    df_all.to_csv(filepath, index=False)
+
+
+if __name__ == "__main__":
+    download()
